f0704ab536b509b089d198902ac994aa68f76165
Mohammed Affan <mohammed@thebimhub.com>
2014-08-06 21:20:36 +0400

    fix for _init_ imports

--- embeds/utils.py
+++ embeds/utils.py
@@ -1,12 +1,14 @@
 import re
-import embeds
+from embeds import EMBEDLY_KEY, EMBED_REGEX, USER_AGENT
+
 from embedly import Embedly
+
 
 def detect_embedded_content(text):
     results = []
 
-    client = Embedly(key=embeds.EMBEDLY_KEY, user_agent=embeds.USER_AGENT)
-    for url in re.findall(embeds.EMBED_REGEX, text):
+    client = Embedly(key=EMBEDLY_KEY, user_agent=USER_AGENT)
+    for url in re.findall(EMBED_REGEX, text):
         results.append(client.oembed(url))
 
     return results
